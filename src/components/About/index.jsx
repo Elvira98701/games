9382b0fd35beb8f9d7dd5e878efa5d4ca6177374
Elvira Latypova <ellalatypova666@gmail.com>
2024-10-27 20:56:26 +0500

    Update favourites

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -27,6 +27,20 @@ const About = () => {
     <section className={styles.about}>
       <div className="container">
         <div className={styles.aboutHeader}>
+          <svg
+            width="60"
+            height="60"
+            viewBox="0 0 64 64"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <path
+              fill="#e0e1dd"
+              d="M38.2 34.6L64 32l-25.8-2.6l16.4-20l-20 16.4L32 0l-2.6 25.8l-20-16.4l16.4 20L0 32l25.8 2.6l-16.4 20l20-16.4L32 64l2.6-25.8l20 16.4z"
+            ></path>
+          </svg>
           <h2 className={styles.aboutTitle}>About us</h2>
           <p className={styles.aboutSlogun}>
             We are a team of avid gamers who know everything about video games.
--- src/components/TopDevelopers/index.jsx
+++ src/components/TopDevelopers/index.jsx
@@ -7,6 +7,20 @@ const TopDevelopers = () => {
     <section className={styles.topDevelopers}>
       <div className="container">
         <div className={styles.topDevelopersInner}>
+          <svg
+            width="50"
+            height="50"
+            viewBox="0 0 64 64"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <path
+              fill="#e0e1dd"
+              d="M38.2 34.6L64 32l-25.8-2.6l16.4-20l-20 16.4L32 0l-2.6 25.8l-20-16.4l16.4 20L0 32l25.8 2.6l-16.4 20l20-16.4L32 64l2.6-25.8l20 16.4z"
+            ></path>
+          </svg>
           <h2>Developers</h2>
           <ul className={styles.topDevelopersList}>
             {developers.map((developer) => (
