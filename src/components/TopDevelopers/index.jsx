ac91a966846489f9b357e66b459592b273e46bc4
Elvira Latypova <ellalatypova666@gmail.com>
2024-10-27 18:31:14 +0500

    Update font

--- src/components/TopDevelopers/index.jsx
+++ src/components/TopDevelopers/index.jsx
@@ -5,15 +5,17 @@ const TopDevelopers = () => {
   const { developers } = useSelector((state) => state.developers);
   return (
     <section className={styles.topDevelopers}>
-      <div className={`${styles.topDevelopersInner} container`}>
-        <h2>Developers</h2>
-        <ul className={styles.topDevelopersList}>
-          {developers.map((developer) => (
-            <li className={styles.topDevelopersItem} key={developer.id}>
-              {developer.name}
-            </li>
-          ))}
-        </ul>
+      <div className="container">
+        <div className={styles.topDevelopersInner}>
+          <h2>Developers</h2>
+          <ul className={styles.topDevelopersList}>
+            {developers.map((developer) => (
+              <li className={styles.topDevelopersItem} key={developer.id}>
+                {developer.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
