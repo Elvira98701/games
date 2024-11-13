7318907ff5c29b3edda21386064bf174517b48ea
Elvira Latypova <ellalatypova666@gmail.com>
2024-11-13 14:29:16 +0500

    Update game card

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -39,21 +39,23 @@ const GameCard = ({
           <h3 className={styles.cardTitle}>{name}</h3>
         </Link>
       </div>
-      <svg
-        className={styles.cardIcon}
-        onClick={onClick}
-        xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
-        fill={
-          favouritesList.findIndex((obj) => obj.id === id) !== -1
-            ? "#d90429"
-            : "#e0e1dd"
-        }
-        viewBox="0 0 256 256"
-      >
-        <path d="M240,102c0,70-103.79,126.66-108.21,129a8,8,0,0,1-7.58,0C119.79,228.66,16,172,16,102A62.07,62.07,0,0,1,78,40c20.65,0,38.73,8.88,50,23.89C139.27,48.88,157.35,40,178,40A62.07,62.07,0,0,1,240,102Z"></path>
-      </svg>
+      <div className={styles.cardFavoutites}>
+        <svg
+          className={styles.cardFavoutitesIcon}
+          onClick={onClick}
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          fill={
+            favouritesList.findIndex((obj) => obj.id === id) !== -1
+              ? "#d90429"
+              : "#e0e1dd"
+          }
+          viewBox="0 0 256 256"
+        >
+          <path d="M240,102c0,70-103.79,126.66-108.21,129a8,8,0,0,1-7.58,0C119.79,228.66,16,172,16,102A62.07,62.07,0,0,1,78,40c20.65,0,38.73,8.88,50,23.89C139.27,48.88,157.35,40,178,40A62.07,62.07,0,0,1,240,102Z"></path>
+        </svg>
+      </div>
     </article>
   );
 };
